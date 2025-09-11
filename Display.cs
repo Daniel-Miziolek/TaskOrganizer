ed50ad942fbc1527a71dda26ffa1ebb6d749c08e
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2025-09-11 22:53:14 +0200

    Add delete task function

--- Display.cs
+++ Display.cs
@@ -7,14 +7,13 @@ public class Display
     public static string MainChoice()
     {
         return AnsiConsole.Prompt(
-                   new SelectionPrompt<string>()
-                       .Title("Choose one of the options")
-                       .PageSize(10)
-                       .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
-                       .AddChoices([
-                        "Add Task", "Update Task", "Delete Task", "Close the program"
-                       ])
-               ); ;
+            new SelectionPrompt<string>()
+                .Title("Choose one of the options")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
+                .AddChoices([
+                    "Add Task", "Update Task", "Delete Task", "Close the program"
+                ]));
     }
 
     public static void DisplayTasksList()
@@ -29,7 +28,7 @@ public class Display
 
         foreach (var task in TaskData.ListOfTasks)
         {
-            table.AddRow(task.Id.ToString(), task.Title, task.Descritpion, task.TaskPriority.ToString(), task.IsFinish.ToString());
+            table.AddRow(task.Id.ToString(), task.Title, task.Description, task.TaskPriority.ToString(), task.IsFinish.ToString());
         }
 
         AnsiConsole.Write(table);
--- Task.cs
+++ Task.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Spectre.Console;
 
 namespace TaskOrganizer;
@@ -6,7 +7,7 @@ public class Task
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string Descritpion { get; set; }
+    public string Description { get; set; }
     public TaskPriority TaskPriority { get; set; }
     public bool IsFinish = false;
 
@@ -17,14 +18,14 @@ public class Task
         Title = AnsiConsole.Prompt(
             new TextPrompt<string>("Enter title of your task: "));
 
-        Descritpion = AnsiConsole.Prompt(
+        Description = AnsiConsole.Prompt(
              new TextPrompt<string>("Enter descrtiption of your task"));
 
         TaskPriority = AnsiConsole.Prompt(
              new SelectionPrompt<TaskPriority>()
                 .Title("Choose priority of your task")
                 .PageSize(10)
-                .MoreChoicesText("Move up and down to reveal more options")
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
                 .AddChoices(Enum.GetValues<TaskPriority>()));
 
         TaskData.ListOfTasks.Add(this);
@@ -37,11 +38,30 @@ public class Task
 
     public void DeleteTask()
     {
-        Console.WriteLine("You choiced DeleteTask option");
+        var ides = TaskData.ListOfTasks
+            .Select(t => t.Id)
+            .ToList();
+
+        var taskDeleteId = AnsiConsole.Prompt(
+            new SelectionPrompt<int>()
+                .Title("Choose task to delete")
+                .MoreChoicesText("[grey](Move up and down to reval more tasks)[/]")
+                .AddChoices(ides));
+
+        TaskData.ListOfTasks.RemoveAll(x => x.Id == taskDeleteId);
+        CheckId();
+    }
+
+    private void CheckId()
+    {
+        for (int i = 0; i < TaskData.ListOfTasks.Count; i++)
+        {
+            TaskData.ListOfTasks[i].Id = i + 1;
+        }
     }
 
     public override string ToString()
     {
-        return $"Id: {Id} Title: {Title}, Descritpion: {Descritpion}, TaskPriority: {TaskPriority}, Is finished: {IsFinish}";
+        return $"Id: {Id} Title: {Title}, Description: {Description}, TaskPriority: {TaskPriority}, Is finished: {IsFinish}";
     }
 }
